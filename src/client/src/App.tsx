d2ff16025a6838b7a35dc0d28285f920c74b44ca
MarkousMalek <MarkousMalek@gmail.com>
2022-04-09 18:49:16 +0200

    simple bootstarp sign in form ui

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import SignUp from "./components/signUp";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Markos</h1>
+      <h1>Hello Marcos</h1>
+      <SignUp />
     </div>
   );
 }
