3bf31d6c95049f544d20470a0341c5a8d571bc8a
MarkousMalek <MarkousMalek@gmail.com>
2022-10-08 06:25:45 +0200

    database is connected locally

---
+++ routes/signUp.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "get user" });
+});
+module.exports = router;
--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 const app = express();
+const connectDB = require("./config/db.js");
 import cors from "cors";
 require("dotenv").config();
 
@@ -7,15 +8,9 @@ app.use(express.static("client"));
 app.use(express.json());
 app.use(cors());
 
+connectDB();
 app.listen(8080, () =>
   console.log("server is listening at http://localhost:8080")
 );
 
-app.get("/", function (req: Request, res: Response) {
-  res.send("This works");
-});
-
-app.post("/data", function (req: Request, res: Response) {
-  console.log(req.body);
-  return res.sendStatus(200);
-});
+app.use("/register", require("./routes/signUp"));
